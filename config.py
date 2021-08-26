b7da529d3eba6437e01508a5d86699e06c79caed
SoleneAcademy <89585833+SoleneAcademy@users.noreply.github.com>
2021-08-26 17:22:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -28,5 +28,7 @@ print("Je me répète mais merci pour ces 6 mois de stage durant lesquels j'ai a
 print("Je repars pour une année d'étude en mastère spécialisé en Cybersécurité à Rennes (si vous avez des bons plans sur Rennes n'hésitez pas d'ailleurs)")
 
 final = "En espérant avoir l'occasion de vous revoir :-) "
+
 print(" intrinsec quement vôtre (petit jeu de mot pour que ça fasse une notif ehehe)")
+print("j'espère avoir laissé mon footprint dans l'équipe (c'est pour augmenter les chances d'avoir une notif ehehe)")
 signature = SGN
