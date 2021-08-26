85cefa433627a4c364ccbe5028a79f2c7bd66096
SoleneAcademy <89585833+SoleneAcademy@users.noreply.github.com>
2021-08-26 16:34:06 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,32 @@
+#fichier de config 
+
+titre = "Merci pour ces 6 mois de stage"
+
+#pas si secrète que ça
+SECRET_KEY = "bGVwcmVtaWVyTjFxdWlhcnJpdmVhZGVjb2RlcmNlbWVzc2FnZWdhZ25ldW5lYmllcmVjZXNvaXI="
+
+if("j'ai passé un super stage"){
+    
+    remercier(all)
+}
+
+DATABASE = {
+    
+    "default" : {
+        
+        "username" : sgn,
+        "password" : 123 ,
+        #pour garder contact
+        "mail" : solene.glavan@gmail.com,
+        "linkedin" : pas encore,
+        "port" : 3306,
+    }
+}
+print("Je me répète mais merci pour ces 6 mois de stage durant lesquels j'ai appris énormement humainement et techniquement. Ca a été très gratifiant de faire partie de l'équipe de CTI et de m'y être sentie aussi intégrée.")
+
+#TODO
+print("Je repars pour une année d'étude en mastère spécialisé en Cybersécurité à Rennes (si vous avez des bons plans sur Rennes n'hésitez pas d'ailleurs)")
+
+final = "En espérant avoir l'occasion de vous revoir :-) "
+print("intrinsec quement vôtre (petit jeu de mot pour que ça fasse une notif ehehe)")
+signature = SGN
